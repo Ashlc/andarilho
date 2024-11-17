e91c092e1b07921a0fadcaa7657e0d967c31c800
Castelo <90470038+Pedr0Castel0@users.noreply.github.com>
2024-11-17 16:58:25 -0300

    refactor: Remover classe de fundo do componente Header

--- front/src/components/Header/index.tsx
+++ front/src/components/Header/index.tsx
@@ -13,7 +13,7 @@ const Index = () => {
     navigate('/login');
   };
   return (
-    <div className="sm:w-screen md:w-10/12 mx-auto md:sticky z-20 bg-white/50 backdrop-blur-sm top-0 py-4 max-md:hidden">
+    <div className="sm:w-screen md:w-10/12 mx-auto md:sticky z-20 top-0 py-4 max-md:hidden">
       <div className="w-full bg-background h-[48px] flex flex-row items-center px-6 rounded-lg border border-b-black select-none justify-between">
         <img src={Andarilho} alt="Logo Andarilho" className="h-4" />
         <Button variant="ghost" size="sm" onClick={logout} className="gap-1">
