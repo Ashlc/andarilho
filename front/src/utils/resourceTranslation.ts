6593a24267a81f261dd8aeb53dca70a23db7e860
Castelo <90470038+Pedr0Castel0@users.noreply.github.com>
2024-11-25 14:20:47 -0300

    feat: adicionar traduções para acessibilidade no recurso de tradução

--- front/src/utils/resourceTranslation.ts
+++ front/src/utils/resourceTranslation.ts
@@ -2,4 +2,9 @@ export const resourceTranslation = {
   wheelchair: 'Rampa de acesso',
   blind: 'Sinalização sonora',
   RAMP: 'Rampa de acesso',
+  tactile_paving: 'Piso tátil',
+  braille_signs: 'Placas em braille',
+  braille_auditory_adaptations: 'Adaptações em braille e sonoras',
+  reserved_parking: 'Vagas reservadas',
+  bathroom_adaptations: 'Banheiro adaptado',
 };
